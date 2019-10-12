8e61c59cb179149619c74060e9df667ef6fdcfc8
Isheeta Chinchankar <isheeta.chinchankar@gmail.com>
2019-10-12 16:19:24 +0530

    added project frontend

---
+++ smart-brain-app/src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3 center'>
+                {'Isheeta, your current rank is '}
+            </div>
+            <div className='white f1 center'>
+                {'#3 '}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
---
+++ smart-brain-app/src/components/Register/Register.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Register = ({ onRouteChange }) => {
+    return (
+        <article className="br3 ba b--black-10 mv3 w-100 w-50-m w-25-l mw6 shadow-5 center">
+            <main className="pa4 black-80 ">
+                <div className="measure">
+                    <fieldset id="register" className="ba b--transparent ph0 mh0 ">
+                        <legend className="f1 fw6 ph0 mh0">Register</legend>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f5" htmlFor="name">Name</label>
+                            <input className="br2 pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="name" name="name" id="name" />
+                        </div>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f5" htmlFor="email-address">Email</label>
+                            <input className="br2 pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
+                        </div>
+                        <div className="mv3">
+                            <label className="db fw6 lh-copy f5" htmlFor="password">Password</label>
+                            <input className="br2 b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
+                        </div>
+                    </fieldset>
+                    <div className="">
+                        <input className="br2 b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f5 dib"
+                            type="submit"
+                            value="Register"
+                            onClick={() => onRouteChange('signin')} />
+                    </div>
+                </div>
+            </main>
+        </article>
+    )
+}
+
+export default Register;
