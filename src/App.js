c6639b7902fbf161d0e510fff4894e99843e16bb
Isheeta Chinchankar <isheeta.chinchankar@gmail.com>
2019-10-16 12:32:49 +0530

    updated fetch

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://peaceful-temple-23523.herokuapp.com:3000/image', {
+          fetch('https://peaceful-temple-23523.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
